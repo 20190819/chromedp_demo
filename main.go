d8c6a474ee1dd7c0c5f74717bbd2b90d40086945
杨亮 <yangliang@114study.com>
2022-06-27 11:26:01 +0800

    编译

--- main.go
+++ main.go
@@ -6,27 +6,26 @@ import (
 	"github.com/antchfx/htmlquery"
 	"github.com/chromedp/chromedp"
 	"github.com/pkg/errors"
+	"io/fs"
 	"log"
 	"os"
 	"time"
 )
 
-var html string
-
-//var pages []string
-var fPath = "./reg.html"
+var htmlContent string
 
 const targetURL = "https://www.ggcx.com/main/globalRegistrar"
+const maxPage = 5
 
 func main() {
 	fmt.Println("start chromedp ...")
-	_, err := Spider()
+	_, err := Spider(maxPage)
 	if err != nil {
 		errors.Wrap(err, "internal err")
 	}
 }
 
-func Spider() (context.Context, error) {
+func Spider(page int) (context.Context, error) {
 	// 禁用chrome headless
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
 		chromedp.Flag("headless", false),
@@ -43,14 +42,14 @@ func Spider() (context.Context, error) {
 		ctx,
 		chromedp.Navigate(targetURL),
 		chromedp.WaitVisible(`//*[@id="main"]/div/div[3]/div[5]/table`),
-		chromedp.Sleep(time.Second),
-		chromedp.OuterHTML(`//*[@id="main"]/div/div[3]/div[5]/table`, &html),
 		chromedp.ActionFunc(func(ctx context.Context) error {
-			for i := 1; i <= 3; i++ {
-				LoadDoc(html)
-				chromedp.Sleep(5 * time.Second).Do(ctx)
+			for i := 0; i <= page; i++ {
+				if i > 0 {
+					chromedp.OuterHTML(`//*[@id="main"]/div/div[3]/div[5]/table`, &htmlContent).Do(ctx)
+					LoadDoc(htmlContent, i)
+				}
+				chromedp.Sleep(3 * time.Second).Do(ctx)
 				chromedp.Click(`//*[@id="main"]/div/div[3]/div[5]/div[3]/div/div[2]`).Do(ctx)
-				fmt.Println(`page+1`)
 
 			}
 			return nil
@@ -59,27 +58,24 @@ func Spider() (context.Context, error) {
 	return ctx, err
 }
 
-func LoadDoc(html string) error {
+func LoadDoc(html string, number int) error {
 	fmt.Println("loadDoc", len(html))
 	var file *os.File
+	var filePath = fmt.Sprintf("./reg_%d.html", number)
+	file, err := os.OpenFile(filePath, os.O_CREATE, fs.ModePerm)
+	if err != nil {
+		fmt.Println(errors.Wrap(err, "打开文件报错"))
+	}
 	defer func() {
 		file.Close()
-		os.Remove(fPath)
+		os.Remove(filePath)
 	}()
-	if _, err := os.Stat(fPath); err != nil {
-		if os.IsNotExist(err) {
-			// file does not exist
-			file, _ = os.Create(fPath)
-		}
-	}
-	n, err := file.WriteString(html)
+	_, err = file.WriteString(html)
 	if err != nil {
-		fmt.Println("write err:", err)
+		fmt.Println(errors.Wrap(err, "写入文件报错"))
 		os.Exit(-1)
 	}
-	fmt.Println("write n:",n)
-	doc, _ := htmlquery.LoadDoc(fPath)
-
+	doc, _ := htmlquery.LoadDoc(filePath)
 	trs := htmlquery.Find(doc, ".//tr")
 	for _, tr := range trs {
 		tds := htmlquery.Find(tr, ".//td")
